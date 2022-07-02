03df6296fdab911d96b7fcbdd80344b60f56b04c
leandroj <leandro.jesus@outlook.com>
2022-07-02 19:48:01 -0300

    Commit Incial

---
+++ posts/teste_pyscript.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(42)
+
+rv = np.random.standard_normal(1000)
+
+fig, ax = plt.subplots()
+ax.hist(rv, bins=30)
+fig
+
+pyscrip.write("main", "asdasd")
